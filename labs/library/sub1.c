6d7b22a0313bdfdd219a3f882459c053cc0399e7
Victor Eijkhout <eijkhout@tacc.utexas.edu>
2020-09-24 13:26:33 -0500

    library lab files

---
+++ labs/library/sub1.c
@@ -0,0 +1,12 @@
+/****************************************************************
+ ****
+ **** sub1.c : declaration of function for fooprog.c
+ ****
+ ****************************************************************/
+#include <stdlib.h>
+#include <stdio.h>
+
+void foo(char *s) {
+  printf("Text one: %s\n",s);
+  return;
+}
